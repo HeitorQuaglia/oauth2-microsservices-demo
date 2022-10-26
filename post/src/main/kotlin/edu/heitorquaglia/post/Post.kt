a3a4a4920112d2d2d517534ba411ccfc529f0253
Heitor Quaglia <heitor.quaglia@digitalrepublic.com.br>
2022-10-26 15:17:13 -0300

    add post module

---
+++ post/src/main/kotlin/edu/heitorquaglia/post/Post.kt
@@ -0,0 +1,10 @@
+package edu.heitorquaglia.post
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Post
+fun main(args: Array<String>) {
+    runApplication<Post>(*args)
+}
---
+++ post/src/test/kotlin/edu/heitorquaglia/post/PostApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.heitorquaglia.post
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PostApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
