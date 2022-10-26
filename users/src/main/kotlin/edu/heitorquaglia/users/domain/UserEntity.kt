4f14db0fe0e3c7c1ae299d51124ca92514f14957
Heitor Quaglia <heitor.quaglia@digitalrepublic.com.br>
2022-10-26 18:43:28 -0300

    feat: add domain classes to user

---
+++ users/src/main/kotlin/edu/heitorquaglia/users/domain/UserEntity.kt
@@ -0,0 +1,27 @@
+package edu.heitorquaglia.users.domain
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.OffsetDateTime
+import javax.persistence.*
+
+
+@Entity
+@EntityListeners(AuditingEntityListener::class)
+data class UserEntity(
+    @Id
+    @GeneratedValue
+    var id: Long,
+    var name: String,
+    @Column(unique = true)
+    var email: String,
+    var password: String,
+    @Enumerated(EnumType.STRING)
+    var type: Type,
+    @CreatedDate
+    var createdAt: OffsetDateTime,
+) {
+    enum class Type {
+        ADMIN, CLIENT
+    }
+}
---
+++ users/src/main/kotlin/edu/heitorquaglia/users/domain/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package edu.heitorquaglia.users.domain.repository
+
+import edu.heitorquaglia.users.domain.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+
+interface UserRepository : JpaRepository<UserEntity, Long> {
+    fun findByEmail(username: String?): Optional<UserEntity>
+}
