dc6e18c82c4334e41486830dbcc1859389d0b1c6
Heitor Quaglia <heitor.quaglia@digitalrepublic.com.br>
2022-10-27 11:29:19 -0300

    feat: add security classes to posts

---
+++ post/src/main/kotlin/edu/heitorquaglia/post/security/SecurityService.kt
@@ -0,0 +1,11 @@
+package edu.heitorquaglia.post.security
+
+import org.springframework.stereotype.Service
+
+@Service
+class SecurityService {
+    //TODO Implementar
+    val userId: Long
+        get() =//TODO Implementar
+            2L
+}
