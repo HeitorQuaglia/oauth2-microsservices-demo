e709eb32ca06d93d16dd0d39eb339dc590fa35c8
Heitor Quaglia <heitor.quaglia@digitalrepublic.com.br>
2022-10-31 18:10:12 -0300

    fix: repository not found

--- auth-server/src/main/kotlin/edu/heitorquaglia/authserver/repositories/UserRepository.kt
+++ auth-server/src/main/kotlin/edu/heitorquaglia/authserver/repositories/UserRepository.kt
@@ -4,6 +4,6 @@ import edu.heitorquaglia.authserver.domain.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
-abstract class UserRepository : JpaRepository<UserEntity, Long> {
-    abstract fun findByEmail(email: String?): Optional<UserEntity>
+interface UserRepository : JpaRepository<UserEntity, Long> {
+    fun findByEmail(email: String?): Optional<UserEntity>
 }
